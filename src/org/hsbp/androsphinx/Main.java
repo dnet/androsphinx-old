fb76a1ddcf0a76c71023b640eff8d24806ea01d5
András Veres-Szentkirályi <vsza@vsza.hu>
2018-06-26 17:26:51 +0200

    use Arrays.fill instead of for loop

--- src/org/hsbp/androsphinx/Main.java
+++ src/org/hsbp/androsphinx/Main.java
@@ -1,11 +1,12 @@
 package org.hsbp.androsphinx;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
 		System.loadLibrary("test");
 		Sphinx.ChallengeResult cr = Sphinx.challenge("shitty password\0".toCharArray());
 		byte[] secret = new byte[32];
-		for (int i = 0; i < secret.length; i++) secret[i] = (byte)' ';
+		Arrays.fill(secret, (byte)' ');
 		byte[] resp = Sphinx.respond(cr.challenge, secret);
 		byte[] rwd = Sphinx.finish(cr.blindingFactor, resp);
 		dump(rwd);
